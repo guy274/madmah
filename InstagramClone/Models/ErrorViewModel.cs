ab773408954d800281fa63405d7f62b47de6812b
Yan <regoner2@gmail.com>
2025-07-29 18:45:33 +0300

    Added mvc template

---
+++ InstagramClone/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace InstagramClone.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ SafeChat/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace SafeChat.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
